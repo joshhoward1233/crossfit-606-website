4373b55feffb179d0561510583432dffc98b22a9
joshhoward1233 <70409615+joshhoward1233@users.noreply.github.com>
2025-11-27 22:31:36 -0500

    Updated images & journal logo
    Updated all images to webp format and changed the location of the journal img

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Automatic Slideshow - change image every 4 seconds
+// Top/Header Automatic Slideshow - change image every 5 seconds
     var myIndex = 0;
     carousel();
 
@@ -11,7 +11,7 @@
         myIndex++;
         if (myIndex > x.length) {myIndex = 1}
         x[myIndex-1].style.display = "block";
-        setTimeout(carousel, 4000);
+        setTimeout(carousel, 5000);
     }
 
 // Used to toggle the menu on small screens when clicking on the menu button
@@ -23,3 +23,34 @@
             x.className = x.className.replace(" w3-show", "");
         }
     }
+
+// Coaches Slideshow
+
+    let slideIndex = 1;
+        showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlideshow");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    } 
